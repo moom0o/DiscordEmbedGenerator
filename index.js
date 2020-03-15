3f1994e3148aa00b9e3a92b74a2f3bd786455333
moom0o <48740106+moom0o@users.noreply.github.com>
2020-03-15 00:04:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.get("/e", function(req, res) {
   if(!req.query.q){
     return res.send("no query")
   }
-  res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${req.query.q}" property="og:description"><meta name="theme-color" content="#28B1FF">`)
+  if(!req.query.color){
+      return res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} and &color=0 to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${req.query.q}" property="og:description">`)
+  }
+  res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} and &color=${req.query.color} to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${req.query.q}" property="og:description"><meta name="theme-color" content="#${req.query.color}">`)
 })
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
