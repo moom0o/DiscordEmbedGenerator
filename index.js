d426357bea291a8fd3b7c0cdbeed21080ef3f710
moom0o <48740106+moom0o@users.noreply.github.com>
2020-03-15 02:06:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ app.use(express.static("public"));
 app.use(express.static("views"));
 app.use(morgan('tiny'))
 app.get("/e", function(req, res) {
-  if(!req.query.q){
-    return res.send("no query")
+  if(!req.query.q && !req.query.color && !req.query.image && !req.query.title && !req.query.tinytitle){
+    return res.status(400).send(`You don't have any valid params! More info in Github.<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="ERROR: No valid params." property="og:description"><meta name="theme-color" content="#9e0303">`)
   }
-  if(!req.query.color){
-      return res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} and &color=0 to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${req.query.q}" property="og:description">`)
-  }
-  res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} and &color=${req.query.color} to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${req.query.q}" property="og:description"><meta name="theme-color" content="#${req.query.color}">`)
+  let description = `${req.query.q}`.replace("undefined", "")
+  let color = `${req.query.color}`.replace("undefined", "")
+  let image = `${req.query.image}`.replace("undefined", "")
+  let title = `${req.query.title}`.replace("undefined", "")
+  let tinytitle = `${req.query.tinytitle}`.replace("undefined", "")
+  res.send(`What is this? This is an embed generator! You can change ?q=${req.query.q} and &color=${req.query.color} (more in github link.) to whatever message you want in discord!<br>Open sourced  https://github.com/moom0o/DiscordEmbedGenerator/<meta content="${description}" property="og:description"><meta name="theme-color" content="#${color}"><meta content="${title}" property="og:title"><meta content="${tinytitle}" property="og:site_name"><meta content='${image}' property='og:image'>`)
 })
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
